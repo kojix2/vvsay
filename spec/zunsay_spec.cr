bad33191e26e72869842cc05d54cddad81d466a2
kojix2 <2xijok@gmail.com>
2025-04-02 20:34:19 +0900

    Make test work

--- spec/zunsay_spec.cr
+++ spec/zunsay_spec.cr
@@ -1,9 +1,7 @@
 require "./spec_helper"
 
 describe Zunsay do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  it "has a version number" do
+    Zunsay::VERSION.should be_a(String)
   end
 end
